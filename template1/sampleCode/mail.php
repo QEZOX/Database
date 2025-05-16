894c8fc294253789d5b9dbbb6b216eb584029fd2
Arash zareian <Arash.zareyian82@gmail.com>
2025-05-16 04:11:31 +0330

    .

--- template1/sampleCode/mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-$to = "takalifmadrese@gmail.com";
-$subject = "موضوع ایمیل";
-$message = "متنی که در ایمیل، نمایش داده خواهد شد";
-$from = "name_2@example.com";
-$headers = "From:" . $from;
-mail($to,$subject,$message,$headers);
-echo "ایمیل ارسال شد";
-?>
